8158676b0fbc81be3631891cfc973f467e41c13e
Caupo Helvik <helvikcaupo@gmail.com>
2020-03-02 13:15:26 +0200

    Resolves #20 Resolves #19 Resolves #18 Resolves #16 Resolves #15 Resolves #14

---
+++ components/MenuPanel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MenuPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div className={"todo"}>
+            </div>
+        )
+    }
+}
+
+export default MenuPanel;
---
+++ components/TabList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class TabList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div className={"todo"}>
+            </div>
+        )
+    }
+}
+
+export default TabList;
